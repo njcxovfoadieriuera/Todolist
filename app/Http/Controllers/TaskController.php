b3d18e8656cc54e7d8a7f4d41917787fbba4086b
suminami <100803reiji@gmail.com>
2023-07-14 12:51:31 +0900

    チャプター4まで

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
         $folders = Folder::all();
+        $current_folder = Folder::find($id);
+        // $tasks = Task::where('folder_id', $current_folder->id)->get();一個下の行と同じことしてる
+        $tasks = $current_folder->tasks()->get();//リレーション組んだ後にできる
 
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }
---
+++ app/Models/Task.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Task extends Model
+{
+    use HasFactory;
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['label'];
+    }
+
+    public function getStatusClassAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
+
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d', $this->attributes['due_date'])
+            ->format('Y/m/d');
+    }
+
+    // protected $table = 'tasks';
+    // protected $fillable = 
+    // [
+    //     'id',
+    //     'filder_id',
+    //     'title',
+    //     'status',
+    //     'due_date',
+    //     'created_at',
+    //     'updated_at',
+    // ];
+
+    // protected $casts = [
+    //     'first_logged_in_at' => 'datetime:Y-m-d',
+    //     'created_at' => 'datetime:Y-m-d H:i:s',
+    //     'updated_at' => 'datetime:Y-m-d H:i:s',
+    // ];
+
+    // public $timestamps = false;
+}
